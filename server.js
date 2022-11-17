163a07f1b277f0a9e664b537afb811ad2ede36c3
Su Shruthi <shruthacm@gmail.com>
2022-11-16 17:21:45 -0800

    added callback for server listen

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log("Here");
+  // console.log("test");
   res.send("Api");
 });
 const userRouter = require("./routes/videos");
 app.use("/videos", userRouter);
 
-app.listen(8080);
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080!");
+});
 
 // "api_key": "c52fc4fb-65e8-407a-a00b-a6df33ac4dae"
