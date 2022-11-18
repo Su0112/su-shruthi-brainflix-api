fe01c42d7f35c9cf1e16b1599d188d8b24712514
Su Shruthi <shruthacm@gmail.com>
2022-11-18 02:52:08 -0800

    added images, videos, utils, changed the link address for the video-details

--- routes/videos.js
+++ routes/videos.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const path = require("node:path");
+//const data = require("../data/video-details.json");
+const { getNewId, writeJSONFile } = require("../utils/utils");
+
+const videosJSONFile = path.join(__dirname, "../data/video-details.json");
+const data = require(videosJSONFile);
+
+router.use(express.json());
 
 router.get("/", (req, res) => {
   //console.log(req.user);
-  res.send("Video List");
+  try {
+    res.json(data);
+  } catch (error) {
+    console.log("Error retrieving the videos", error);
+  }
 });
+
 router.get("/:id", (req, res) => {
-  res.send("Video id");
+  const { id } = req.params;
+
+  const video = data.find((v) => {
+    return v.id === id;
+  });
+  if (video) {
+    res.json(video);
+  } else {
+    res.json({ error: `Video with ID ${re.params.id} not found` });
+  }
 });
+
 router.post("/", (req, res) => {
   res.send("Video Post");
 });
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+//require("dotenv").config();
 const express = require("express");
+const path = require("node:path");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+//middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   // console.log("test");
@@ -11,5 +19,3 @@ app.use("/videos", userRouter);
 app.listen(8080, () => {
   console.log("Server is listening on port 8080!");
 });
-
-// "api_key": "c52fc4fb-65e8-407a-a00b-a6df33ac4dae"
